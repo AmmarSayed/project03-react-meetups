ef41ce120f599ce05bbcf029ff34e5adb7affd1c
Ammar Sayed <ammarsayed1988@gmail.com>
2022-07-30 14:22:37 +0200

    end of lesson 203

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -1,10 +1,50 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [err, setErr] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputRef = useRef("");
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setErr(true);
+      return;
+    }
+    setErr(false);
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid = enteredNameTouched && err;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
+      {nameInputIsInvalid && (
+        <span className="error-text">Please enter valid a name</span>
+      )}
       <div className="form-actions">
         <button>Submit</button>
       </div>
