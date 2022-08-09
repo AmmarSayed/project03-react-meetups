d5cb1969eda28d1c89f06a65f867b7dd14d126c9
Ammar Sayed <ammarsayed1988@gmail.com>
2022-08-09 23:50:03 +0200

    end of section20

--- 20-2-react-router-practice/src/App.js
+++ 20-2-react-router-practice/src/App.js
@@ -1,8 +1,32 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQoutes from "./pages/AllQuotes";
+import QuoteDetails from "./pages/QuoteDetails";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQoutes />
+          </Route>
+          <Route path="/quotes/:qid">
+            <QuoteDetails />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
--- 20-2-react-router-practice/src/components/quotes/NoQuotesFound.js
+++ 20-2-react-router-practice/src/components/quotes/NoQuotesFound.js
@@ -1,12 +1,12 @@
-import classes from './NoQuotesFound.module.css';
-
+import classes from "./NoQuotesFound.module.css";
+import { Link } from "react-router-dom";
 const NoQuotesFound = () => {
   return (
     <div className={classes.noquotes}>
-      <p>No quotes found!</p>
-      <a className='btn'>
+      <p>No quote found!</p>
+      <Link to="/new-quote" className="btn">
         Add a Quote
-      </a>
+      </Link>
     </div>
   );
 };
