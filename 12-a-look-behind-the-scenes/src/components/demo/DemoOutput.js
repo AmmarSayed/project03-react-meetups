c4ded79a6dabb92e32de008e062ce869c0c2f115
Ammar Sayed <ammarsayed1988@gmail.com>
2022-08-14 21:40:06 +0200

    Ready for deployment

--- 12-a-look-behind-the-scenes/src/components/demo/DemoOutput.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-import MyParagraph from "./MyParagraph";
-
-const DemoOutput = (props) => {
-  console.log("demo output");
-  return <MyParagraph>{props.show ? "This is New!" : ""}</MyParagraph>;
-};
-
-export default memo(DemoOutput);
--- 12-a-look-behind-the-scenes/src/components/demo/MyParagraph.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MyParagraph = (props) => {
-  console.log("demo paragraph");
-  return <p>{props.children}</p>;
-};
-
-export default MyParagraph;
